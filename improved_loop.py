191203b8d8cd0dbb9c0458ff377e55bbc30af93c
Josh <josh.j.howell@gmail.com>
2017-06-15 19:48:32 -0700

    Added simple python loop script

---
+++ improved_loop.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(7,GPIO.OUT)
+
+armed = 1
+
+while armed > 0:
+  print "LED on"
+  GPIO.output(7,GPIO.HIGH)
+  time.sleep(5)
+  print "LED off"
+  GPIO.output(7,GPIO.LOW)
+  time.sleep(5)
+
