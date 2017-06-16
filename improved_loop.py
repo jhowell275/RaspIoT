83e0ff6ba1a288f5e3f6f280103047ee87d0cf97
nobody1234 <josh.j.howell@gmail.com>
2017-06-16 12:16:12 -0700

    Improving program flow

--- improved_loop.py
+++ improved_loop.py
@@ -4,12 +4,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(7,GPIO.OUT)
 
-armed = 1
+True = 1
 
-while armed > 0:
-  print "LED on"
-  GPIO.output(7,GPIO.HIGH)
-  time.sleep(6)
-  print "LED off"
-  GPIO.output(7,GPIO.LOW)
-  time.sleep(5)
+while True > 0:
+  print "am inside the loop"
+  time.sleep(3)
+
+
+  # print "LED on"
+  # GPIO.output(7,GPIO.HIGH)
+  # time.sleep(6)
+  # print "LED off"
+  # GPIO.output(7,GPIO.LOW)
+  # time.sleep(5)
